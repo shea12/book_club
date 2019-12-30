5536782b2e98436765d9ee7b80c5612ed058a6af
Caroline Shea <caroline@portalmedia.com>
2019-12-30 13:29:34 -0600

    update routes

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/widgets.dart';
-import 'package:book_club/screens/home/home.dart';
-import 'package:book_club/screens/club/clubscreen.dart';
+import 'package:book_club/screens/wrapper.dart';
 
 final Map<String, WidgetBuilder> routes = <String, WidgetBuilder>{
-  "/": (BuildContext context) => Home(),
-  "/club": (BuildContext context) => ClubScreen(),
+  "/": (BuildContext context) => Wrapper()
 };
